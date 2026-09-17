072d9cee51c31a87adba1c3d89e496dff2b24069
seong929 <rlgur0929@naver.com>
2026-09-17 21:51:20 +0900

    todolist구성

--- lib/view/todolist.dart
+++ lib/view/todolist.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:http/http.dart' as http;
+
+import 'insert_todolist.dart';
 
 class Todolist extends StatefulWidget {
   const Todolist({super.key});
@@ -9,55 +13,106 @@ class Todolist extends StatefulWidget {
 }
 
 class _TodolistState extends State<Todolist> {
-  List todoList = [];
+  // Property
+  List data = [];
 
   @override
   void initState() {
     super.initState();
-    //
+    getTodoList();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo List'),
+        title: const Text('Todo List 검색'),
+        centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {
-              //Get
-            }, 
-            icon: Icon(Icons.delete_outline)),
-          IconButton(
-            onPressed: () {
-              //showAddDialog();
-            }, 
-            icon: Icon(Icons.add_outlined)),
+            onPressed: () async {
+              var result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const InsertTodolist();
+                  },
+                ),
+              );
+
+              if (result == true) {
+                getTodoList();
+              }
+            },
+            icon: const Icon(
+              Icons.add,
+            ),
+          ),
         ],
       ),
-      body:Center(
-        child: todoList.isEmpty
-        ? Text('등록된 정보가 없습니다.')
-        : ListView.builder(
-          itemCount: todoList.length,
-          itemBuilder: (context, index) {
-            return Slidable(
-              key: ValueKey(todoList[index]['seq']),
-              startActionPane:  ActionPane(
-                motion: DrawerMotion(),
-                extentRatio: 0.25,
-                children: [
-                  SlidableAction(
-                    onPressed: (context) {
-                      //
-                    },
-                  ),
-                ]
+
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(10, 5, 10, 5,),
+            child: Container(
+              height: 70,
+              decoration: BoxDecoration(
+                color: index % 2 == 0
+                    ? Colors.amber[100]
+                    : Colors.pink[100],
+                borderRadius: BorderRadius.circular(10),
               ),
-              child: Card()
-            );
-          },
-        ),
-      )
+              child: Row(
+                children: [
+                  SizedBox(width: 10,),
+                  // Todo 이미지
+                  ClipOval(
+                    child: Image.network(
+                      getImageUrl(data[index]['seq']),
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.cover,
+                      errorBuilder: (
+                        context,
+                        error,
+                        stackTrace,
+                      ) {
+                        return Icon(
+                          Icons.image,
+                          size: 50,
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 15,),
+                  Expanded(
+                    child: Text(
+                      '${data[index]['title']} / '
+                      '${data[index]['added_date']}',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
+  } // build
+
+  Future<void> getTodoList() async {
+    var url = Uri.parse('http://192.168.10.46:8000/todo/select_todo',);
+    var response = await http.get(url);
+    var result = jsonDecode(utf8.decode(response.bodyBytes),);
+    data = result['results'];
+    setState(() {});
   }
-}
+
+  String getImageUrl(int seq) {
+    return 'http://192.168.10.46:8000/todo/select_image$seq';
+  }
+
+
+} // class
