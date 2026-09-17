2196c1f390cb7cd89042f76f3028c84b22830423
lgh877 <lgh877@naver.com>
2026-09-17 19:58:00 +0900

    gasdf

--- python/todolist.py
+++ python/todolist.py
@@ -2,14 +2,14 @@ from fastapi import FastAPI
 from insert_todo import router as insert_router
 from delete_todo import router as delete_router
 from select_todo import router as select_router
-from selectdeleted_todo import router as selectdeleted_router
+#from selectdeleted_todo import router as selectdeleted_router
 
 app = FastAPI(title="Todo API")
 
 app.include_router(insert_router, prefix="/todo", tags=["todo"])
 app.include_router(delete_router, prefix="/todo", tags=["todo"])
 app.include_router(select_router, prefix="/todo", tags=["todo"])
-app.include_router(selectdeleted_router, prefix="/todo", tags=["todo"])
+#app.include_router(selectdeleted_router, prefix="/todo", tags=["todo"])
 
 if __name__ == "__main__":
     import uvicorn
